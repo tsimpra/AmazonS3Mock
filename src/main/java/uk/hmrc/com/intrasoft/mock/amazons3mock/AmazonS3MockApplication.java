04803dc9b42e385ce3462eb2e7f8ba8840099300
vtsimpragakis <vtsimpra@netcompany-intrasoft.com>
2023-04-11 16:41:05 +0300

    init config for creating buckets

--- src/main/java/uk/hmrc/com/intrasoft/mock/amazons3mock/AmazonS3MockApplication.java
+++ src/main/java/uk/hmrc/com/intrasoft/mock/amazons3mock/AmazonS3MockApplication.java
@@ -1,16 +1,63 @@
 package uk.hmrc.com.intrasoft.mock.amazons3mock;
 
 import com.adobe.testing.s3mock.S3MockApplication;
-import com.adobe.testing.s3mock.S3MockConfiguration;
-import org.springframework.boot.SpringApplication;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.GetBucketLocationRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class AmazonS3MockApplication {
 
     public static void main(String[] args) {
         S3MockApplication.start(args);
+        initConfiguration();
+    }
+
+    private static void initConfiguration() {
+        String eisBucket = "a-bucket";
+        String ermisBuket = "another-bucket";
+        createBucket(eisBucket);
+        createBucket(ermisBuket);
+    }
+
+    private static void createBucket(String bucketName){
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider())
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "http://localhost:4566/",
+                                Regions.DEFAULT_REGION.getName()
+                        ))
+                .withPathStyleAccessEnabled(true)
+                .build();
+        try {
+            if (!s3Client.doesBucketExistV2(bucketName)) {
+                // Because the CreateBucketRequest object doesn't specify a region, the
+                // bucket is created in the region specified in the client.
+                s3Client.createBucket(new CreateBucketRequest(bucketName));
+
+                // Verify that the bucket was created by retrieving it and checking its location.
+                String bucketLocation = s3Client.getBucketLocation(new GetBucketLocationRequest(bucketName));
+                System.out.println("Bucket location: " + bucketLocation);
+            } else {
+                return ;
+            }
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it and returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
     }
 
 }
